b89dc8c712d2110a666d2871051591ad5f1eef2b
Ian Flynn <himitsu.monban@gmail.com>
2023-09-20 00:05:37 -0400

    beginnings of a REPL

---
+++ cmd/lispian/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/monban/lispian/interpreter"
+	"github.com/monban/lispian/lexer"
+	"github.com/monban/lispian/parser"
+)
+
+func main() {
+	fmt.Println("Welcome to the Lispian REPL!")
+	scanner := bufio.NewScanner(os.Stdin)
+	l := lexer.Lexer{}
+	for {
+		fmt.Printf("> ")
+		scanner.Scan()
+		input := scanner.Text()
+		if input == "quit" {
+			break
+		}
+		err := l.WriteString(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Printf("DEBUG: tokens: %#v\n", l.Tokens())
+
+		program, err := parser.Parse(l.Tokens())
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Printf("DEBUG: program: %#v\n", l)
+
+		output := interpreter.Eval(program)
+		fmt.Printf("%v\n", output)
+		l.Reset()
+	}
+}
